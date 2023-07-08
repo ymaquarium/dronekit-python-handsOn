cfddcec79a6681e99563d6def6d553f5d538053e
Aquan <34762859+ymaquarium@users.noreply.github.com>
2023-07-08 15:10:35 +0900

    Update flightExperience.py

--- flightExperience.py
+++ flightExperience.py
@@ -27,22 +27,28 @@ try:
     vehicle.wait_for_armable()
     vehicle.wait_for_mode("GUIDED")
     vehicle.armed = True
+    while not vehicle.armed:
+        time.sleep(1)
 except TimeoutError as takeoffError:
     print("Takeoff is timeout!!")
 
 print("Changed to 'GUIDED'\n")
 
+#option change tempolary
+current_option = vehicle.parameters['AUTO_OPTIONS']
+vehicle.parameters['AUTO_OPTIONS'] = 3
+
 #mission clear
 cmds.clear()
 
 #Define mission
-##cmd1 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0,0,0,0,0,0,35.877272, 140.336378,10)
-cmd2 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0,0,0,0,0,0,35.87889456912811, 140.33935282716024,10)
-cmd3 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0,0,0,0,0,0,35.87888424604674, 140.33965994007255,10)
-cmd4 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0,0,0,0,0,0,35.87906858545532, 140.339318291779,10)
-cmd5 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LAND, 0,0,0,0,0,0,35.87906858545532, 140.339318291779,0)
+cmd1 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0,0,0,0,0,0,0,0,10)
+cmd2 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0,0,0,0,0,0,35.878812598463035, 140.3394132748464,10)
+cmd3 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0,0,0,0,0,0,35.87862092608203, 140.33938888754727,10)
+cmd4 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0,0,0,0,0,0,35.878583381953064, 140.33916208566507,10)
+cmd5 = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LAND, 0,0,0,0,0,0,35.878583381953064, 140.33916208566507,0)
 
-##cmds.add(cmd1)
+cmds.add(cmd1)
 cmds.add(cmd2)
 cmds.add(cmd3)
 cmds.add(cmd4)
@@ -51,17 +57,18 @@ cmds.upload()
 
 
 print("---!!!Mission Start!!!---")
-vehicle.simple_takeoff(10)
-#vehicle.commands.next = 0
-time.sleep(10)
+vehicle.commands.next = 0
 vehicle.wait_for_mode("AUTO")
 
 #Notify mission complete
 cmdnext = 0
-while cmdnext - cmds.next != 3:
+while cmdnext - cmds.next != 4:
     if cmdnext != cmds.next:
         print("commands.next :%s"%cmds.next)
     cmdnext = cmds.next
     time.sleep(1)
+
+# option change
+vehicle.parameters["AUTO_OPTIONS"] = current_option
 
 print("\n---!!!MISSION FINISHED!!!---")
